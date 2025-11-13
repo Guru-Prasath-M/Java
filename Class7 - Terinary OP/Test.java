dc2246165fc7013b58956a6973dcea99ec1748d7
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-11-13 13:33:00 +0530

    Terinary Operator

---
+++ Class7 - Terinary OP/Test.java
@@ -0,0 +1,7 @@
+class Test{
+    public static void main(String[]args){
+        int esal = 45000;
+        String msg = esal >= 45000 ? "Eligible" : "Not eligible";
+        System.out.println(msg); // Eligible
+    }
+}
