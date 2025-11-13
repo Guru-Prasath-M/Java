81bf9350a39e92c3e963519c14c07097a453fd07
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-11-13 13:14:58 +0530

    Incr & Decr

---
+++ Class8 - Incr & Decr/Test.java
@@ -0,0 +1,8 @@
+class Test{
+    public static void main(String[]args){
+        int a = 10;
+        int b = ++a; //First Incremant and Then Assign
+        System.out.println(a); //11
+        System.out.println(b); //11
+    }
+}
---
+++ Class8 - Incr & Decr/TestOne.java
@@ -0,0 +1,8 @@
+class Test{
+    public static void main(String[]args){
+        int a = 10;
+        int b = a++; //First assign and Then Increment
+        System.out.println(a); //11
+        System.out.println(b); //10
+    }
+}
