1b02af7927582c7c675f8ad8ce23949dd4e47110
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-11-13 13:21:53 +0530

    Logical Operator

---
+++ Class7 - Logical OP/Test.java
@@ -0,0 +1,7 @@
+class Test{
+    public static void main(String[] args) {
+        String ename = "Guru";
+        int eid = 50000; 
+        System.out.println(ename == "Guru" && eid>=40000); //True -- If anyone of the value is false the output will be false
+    }
+}
