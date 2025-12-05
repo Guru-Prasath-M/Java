8bf4cdac4a7e5599cf228002d23a2038785dae79
Guru-Prasath-M <guruprasath908018@gmail.com>
2025-12-05 12:08:50 +0530

    Supper

---
+++ Class21 - Super/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        SA sa1 = new SA(101, "Rahul", "rg@gmail.com", 4000);
+        sa1.cal_Bal();
+        CA ca1 = new CA(102, "Sonia", "sg@gmail.com", 40000);
+        ca1.cal_Bal();
+    }
+}
